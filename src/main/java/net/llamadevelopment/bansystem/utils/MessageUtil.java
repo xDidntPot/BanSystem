0d9511ab8c9b10d152e577dd5343ce402ade226b
Mundschutziii <37897528+Mundschutziii@users.noreply.github.com>
2019-12-12 19:53:11 +0100

    Add files via upload
    Added an advanced ban and mute help message.

--- src/main/java/net/llamadevelopment/bansystem/utils/MessageUtil.java
+++ src/main/java/net/llamadevelopment/bansystem/utils/MessageUtil.java
@@ -1,5 +1,6 @@
 package net.llamadevelopment.bansystem.utils;
 
+import cn.nukkit.command.CommandSender;
 import net.llamadevelopment.bansystem.BanSystem;
 
 public class MessageUtil {
@@ -19,6 +20,34 @@ public class MessageUtil {
         str = str.replace("%id%", id);
         str = str.replace("%time%", time);
         str = str.replace("%banner%", banner);
+        return str.replace("&", "§");
+    }
+
+    public static void sendBanHelp(CommandSender sender, BanSystem plugin) {
+        int i = plugin.getConfig().getInt("BanReasons.Count");
+        for (int p = 1; p < i; p++) {
+            sender.sendMessage(plugin.getConfig().getString("Prefix").replace("&", "§") + convertBanHelp(String.valueOf(p), plugin.getConfig().getString("BanReasons." + p + ".Reason")));
+        }
+    }
+
+    public static void sendMuteHelp(CommandSender sender, BanSystem plugin) {
+        int i = plugin.getConfig().getInt("MuteReasons.Count");
+        for (int p = 1; p < i; p++) {
+            sender.sendMessage(plugin.getConfig().getString("Prefix").replace("&", "§") + convertMuteHelp(String.valueOf(p), plugin.getConfig().getString("MuteReasons." + p + ".Reason")));
+        }
+    }
+
+    private static String convertBanHelp(String id, String reason) {
+        String str = BanSystem.getInstance().getConfig().getString("BanReasonFormat");
+        str = str.replace("%reason%", reason);
+        str = str.replace("%id%", id);
+        return str.replace("&", "§");
+    }
+
+    private static String convertMuteHelp(String id, String reason) {
+        String str = BanSystem.getInstance().getConfig().getString("MuteReasonFormat");
+        str = str.replace("%reason%", reason);
+        str = str.replace("%id%", id);
         return str.replace("&", "§");
     }
 }
